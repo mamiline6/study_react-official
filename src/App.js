efbc4732f51f87556bf9696317069663b4f267ee
mamiline6 <ohmamirineoo6@gmail.com>
2023-11-07 17:17:55 +0900

    修正：不要なスペースを削除

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-    if(squares [i]) {
+    if(squares[i]) {
       return;
     }
     const nextSquares = squares.slice();
